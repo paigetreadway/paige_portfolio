d0d57ad575cac152250aab5b3b62849fae1d68a4
denni <dennismdan@gmail.com>
2022-02-13 11:02:46 -0500

    updated css file

--- src/components/ExperienceHeader.jsx
+++ src/components/ExperienceHeader.jsx
@@ -2,17 +2,11 @@ import React from "react";
 
 function ExperienceHeader(props) {
   return (
-    <div className="experience-header">
+    <div class="experience-header">
         <h2 class="font-weight-light">{props.position}</h2>
-        <p>
-        {props.companyName}
-        </p>
-        <p>
-        {props.location}
-        </p>
-        <p>
-        {props.dates}
-        </p>
+        <p>{props.companyName}</p>
+        <p>{props.location}</p>
+        <p>{props.dates}</p>
     </div>
   );
 }
